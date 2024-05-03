3133f70c6404ab279c16091cf555a83a982e9373
gabriel.ferreira <gabriel.ferreira@cits.br>
2024-05-03 17:14:47 -0300

    Add node and files inside

---
+++ Docker/nginx_com_nodejs/node/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const app = express()
+
+const port = 3000
+
+const config = {
+
+    host: 'db',
+
+    user: 'root',
+
+    password: 'root',
+
+    database: 'nodedb'
+
+};
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+
+
+const sql = `INSERT INTO people(name) values ('Gabriel')`
+
+connection.query(sql)
+
+connection.end()
